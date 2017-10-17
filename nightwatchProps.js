195a2d09412181658bed9c629be6c653197c3a32
Orton <zackorton@rock.local>
2017-10-17 15:49:09 -0600

    idk

--- nightwatchProps.js
+++ nightwatchProps.js
@@ -1,5 +1,5 @@
 module.exports = {
-    resourcePath : "/Users/ajlarson/src/testing-resources/selenium-resources/",
+    resourcePath : "/Users/zackorton/Documents/DEVMountion/servers/",
     seleniumServer: "selenium-server-standalone-3.6.0.jar",
     chromedriver: "chromedriver"
 }
