4134602ddf162f963e890a3d56bc398cb447db42
Tucker Weathers <tuckw@Tuckers-MacBook-Pro.local>
2017-10-17 15:17:35 -0600

    Edited employee info.

--- nightwatch/test_data/data.js
+++ nightwatch/test_data/data.js
@@ -1,3 +1,74 @@
 module.exports = {
-    foo : 'bar'
+    employees: {
+        existingEmployeeData: {
+           
+
+        },
+
+        editedEmployeeData: {
+            employeeOne: {
+                name: 'Ronald Swanson',
+                id: '1',
+                phoneNumber: '0000000000',
+                title: 'Director of Parks and Rec.'
+            },
+            employeeTwo: {
+                name: 'Leslie Knope',
+                id: '2',
+                phoneNumber: '1234567890',
+                title: 'Deputy Director of Parks and Rec.'
+            },
+            employeeThree: {
+                name: 'April Ludgate',
+                id: '3',
+                phoneNumber: '6666666666',
+                title: 'Mortician',
+            },
+            employeeFour: {
+                name: 'Donna Meagle',
+                id: '4',
+                phoneNumber: '9876543210',
+                title: 'Realtor'
+            },
+            employeeFive: {
+                name: 'Tom Haveford',
+                id: '5',
+                phoneNumber: '5555557924',
+                title: 'The Fresh Maker'
+            },
+            employeeSix: {
+                name: 'Andrew Dwyer',
+                id: '6',
+                phoneNumber: '',
+                title: 'Shoe Shine Boy',
+            },
+            employeeSeven: {
+                name: 'Jerry Girgich',
+                id: '7',
+                phoneNumber: '327288225',
+                title: ''
+            },
+            employeeEight: {
+                name: 'Burt Macklin',
+                id: '8',
+                phoneNumber: '911',
+                title: 'FBI'
+            },
+            //Phone Number too long.
+            empoyeeNine: {
+                name: 'Benjamin Wyatt',
+                id: '9',
+                phoneNumber: '14351239876',
+                title: 'The Arbiter'
+            },
+            //Missing name. Can you guess who?
+            employeeTen: {
+                name: '',
+                id: '10',
+                phoneNumber: '',
+                title: 'Librarian'
+            }
+        }
+    }
+
 }
--- nightwatchProps.js
+++ nightwatchProps.js
@@ -1,5 +1,5 @@
 module.exports = {
-    resourcePath : "/Users/ajlarson/src/testing-resources/selenium-resources/",
-    seleniumServer: "selenium-server-standalone-3.6.0.jar",
+    resourcePath: "/Users/tuckw/Desktop/Selenium Resources/",
+    seleniumServer: "selenium-server-standalone-3.5.0.jar",
     chromedriver: "chromedriver"
 }
