1575cb25c46199c932f2b74e2027ecb287b37baf
Tucker Weathers <tucker.weathers.127@gmail.com>
2017-10-18 09:33:10 -0600

    click by name function

--- nightwatch/test_data/functions.js
+++ nightwatch/test_data/functions.js
@@ -1,31 +1,40 @@
 const selectors = require('./selectors')
 
 const listCheck = (employeeNames, browser) => {
-// checks a list of employeeNames against the names on the screen.
-// e.g. ['Ron Swanson', 'Todd Bergeron',...]
-// should check browser for each name in the right position
+    // checks a list of employeeNames against the names on the screen.
+    // e.g. ['Ron Swanson', 'Todd Bergeron',...]
+    // should check browser for each name in the right position
 }
 
 const clickByName = (employeeName, browser) => {
-// clicks the employee tag in the employee list for the name passed in
-// this will populate the editor
-}
+    // clicks the employee tag in the employee list for the name passed in
+    // this will populate the editor
+    let object = selectors.employeeList
+    for (var card in object) {
+        if (object.hasOwnProperty(card)) {
+            var element = object[card]
+            browser.getText(element, result => {
+                if(result===employeeName)
+                    browser.click(element)
+            })
+        }
+    }
 
-const editorCheck = (employee, browser) => {
-// takes the passed in employee object and compares against what is in
-// the fields
-}
+    const editorCheck = (employee, browser) => {
+        // takes the passed in employee object and compares against what is in
+        // the fields
+    }
 
-const editFields = (fields, values, browser) => {
-// edits the editor fields, based on fields named in the field array, and
-// inputs the values from the value array at the same index
-// e.g. ['nameField', 'titleField'], ['Ron Swanson', 'Director of Parks and Rec.']
-//      would put Ron Swanson in the name field, Director... in the title field
-}
+    const editFields = (fields, values, browser) => {
+        // edits the editor fields, based on fields named in the field array, and
+        // inputs the values from the value array at the same index
+        // e.g. ['nameField', 'titleField'], ['Ron Swanson', 'Director of Parks and Rec.']
+        //      would put Ron Swanson in the name field, Director... in the title field
+    }
 
-module.exports = {
-    listCheck: listCheck,
-    clickByName: clickByName,
-    editFields: editFields,
-    editorCheck: editorCheck
-}
+    module.exports = {
+        listCheck: listCheck,
+        clickByName: clickByName,
+        editFields: editFields,
+        editorCheck: editorCheck
+    }
