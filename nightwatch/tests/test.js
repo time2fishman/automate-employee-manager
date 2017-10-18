318260ff64a93830dd5bbe95ccecd9327e5a2853
Michael Bossart <32489711+MichaelBossart@users.noreply.github.com>
2017-10-18 09:18:53 -0600

    add function

--- nightwatch/tests/test.js
+++ nightwatch/tests/test.js
@@ -3,16 +3,32 @@ const selectors = require('../test_data/selectors')
 const data = require('../test_data/data')
 
 module.exports = {
-    beforeEach : browser => {
+    beforeEach: browser => {
         browser
             .url('http://localhost:3000')
     },
-    after : browser => {
+    after: browser => {
         browser.end()
     },
-    'test' : browser => {
+    'test': browser => {
         console.log(data.foo)
         browser
             .expect.element('body').to.be.present;
+    },
+    'Check that Simulated Employee List Loads': browser => {
+        let employeeNames = [
+            data.employees.existingEmployeeData.employee1.name,
+            data.employees.existingEmployeeData.employee2.name,
+            data.employees.existingEmployeeData.employee3.name,
+            data.employees.existingEmployeeData.employee4.name,
+            data.employees.existingEmployeeData.employee5.name,
+            data.employees.existingEmployeeData.employee6.name,
+            data.employees.existingEmployeeData.employee7.name,
+            data.employees.existingEmployeeData.employee8.name,
+            data.employees.existingEmployeeData.employee9.name,
+            data.employees.existingEmployeeData.employee10.name
+        ]
+        functions.listCheck(employeeNames, browser)
+
     }
 }
