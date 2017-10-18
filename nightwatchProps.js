4ab7accae285701e4304939938a484d75eb62125
AJ Larson <aj.larson@devmounta.in>
2017-10-18 09:57:38 -0600

    updating cache

--- nightwatchProps.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    resourcePath: 'C:\\Users\\Teri Johannah\\Desktop\\DevmountainProjects\\testing repositeries\\',
-   seleniumServer: "selenium-server-standalone-3.6.0.jar",
-     chromedriver: "chromedriver.exe"
-}
