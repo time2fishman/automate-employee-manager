1020d5021e307e76a43cb4cbcd81f07fba3fe233
Reese <connorreese@MacBook-Pro-7.local>
2017-10-18 09:30:10 -0600

    editorcheck field

--- nightwatch/test_data/functions.js
+++ nightwatch/test_data/functions.js
@@ -4,6 +4,16 @@ const listCheck = (employeeNames, browser) => {
 // checks a list of employeeNames against the names on the screen.
 // e.g. ['Ron Swanson', 'Todd Bergeron',...]
 // should check browser for each name in the right position
+  browser.expect.element(selectors.employeeList.employee1).text.to.equal(employeeNames[0])
+  browser.expect.element(selectors.employeeList.employee2).text.to.equal(employeeNames[1])
+  browser.expect.element(selectors.employeeList.employee3).text.to.equal(employeeNames[2])
+  browser.expect.element(selectors.employeeList.employee4).text.to.equal(employeeNames[3])
+  browser.expect.element(selectors.employeeList.employee5).text.to.equal(employeeNames[4])
+  browser.expect.element(selectors.employeeList.employee6).text.to.equal(employeeNames[5])
+  browser.expect.element(selectors.employeeList.employee7).text.to.equal(employeeNames[6])
+  browser.expect.element(selectors.employeeList.employee8).text.to.equal(employeeNames[7])
+  browser.expect.element(selectors.employeeList.employee9).text.to.equal(employeeNames[8])
+  browser.expect.element(selectors.employeeList.employee10).text.to.equal(employeeNames[9])
 }
 
 const clickByName = (employeeName, browser) => {
@@ -14,8 +24,10 @@ const clickByName = (employeeName, browser) => {
 const editorCheck = (employee, browser) => {
 // takes the passed in employee object and compares against what is in
 // the fields
-browser.expect.element(data.employees.existingEmployeeData.employee1).text.to.equal(selectors.employeeList.employee1)
-
+browser.expect.element(selectors.infoCardList.nameField).text.to.equal(employee.name)
+browser.expect.element(selectors.infoCardList.id).text.to.equal(employee.id)
+browser.expect.element(selectors.infoCardList.phoneNumberField).text.to.equal(employee.phoneNumber)
+browser.expect.element(selectors.infoCardList.titleLabel).text.to.equal(employee.title)
 }
 
 const editFields = (fields, values, browser) => {
