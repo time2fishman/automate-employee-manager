2c75a7df43436b691a2d3946ebc7cc9bcbcadf2d
Teri Larson <terijolarson2016@gmail.com>
2017-10-18 09:32:08 -0600

    edited cancel button test

--- nightwatch/tests/test.js
+++ nightwatch/tests/test.js
@@ -3,16 +3,22 @@ const selectors = require('../test_data/selectors')
 const data = require('../test_data/data')
 
 module.exports = {
-    beforeEach : browser => {
+    beforeEach: browser => {
         browser
             .url('http://localhost:3000')
     },
-    after : browser => {
+    after: browser => {
         browser.end()
     },
-    'test' : browser => {
+    'test': browser => {
         console.log(data.foo)
         browser
             .expect.element('body').to.be.present;
+    },
+    'cancel button does not save edited info': browser => {
+        functions.clickByName(data.employees.existingEmployeeData.employee4.name, browser)
+        functions.editFields(['nameField', 'phoneField', 'titleField'], [data.employees.editedEmployeeData.employee3.name, data.employees.editedEmployeeData.employee3.phoneNumber, data.employees.editedEmployeeData.employee3.title], browser)
+        browser.click(selectors.infoCardItems.clearButton)
+        functions.editorCheck(['nameField', 'phoneField', 'titleField'], [data.employees.existingEmployeeData.employee4.name, data.employees.existingEmployeeData.employee4.phoneNumber, data.employees.existingEmployeeData.employee4.title])
     }
 }
